1ee849e45dde7e0b74334bd21c69c47f7c2cd48f
mancinigabriel <mancinigabriel@hotmail.com>
2021-03-20 20:48:21 -0300

    nova anve

--- examples/spaceinvaders/ship.cpp
+++ examples/spaceinvaders/ship.cpp
@@ -19,11 +19,11 @@ void Ship::initializeGL(GLuint program) {
   // clang-format off
   std::array<glm::vec2, 24> positions{
       // Ship body
-      glm::vec2{-02.5f, +12.5f}, glm::vec2{-15.5f, +02.5f},
-      glm::vec2{-15.5f, -12.5f}, glm::vec2{-09.5f, -07.5f},
-      glm::vec2{-03.5f, -12.5f}, glm::vec2{+03.5f, -12.5f},
-      glm::vec2{+09.5f, -07.5f}, glm::vec2{+15.5f, -12.5f},
-      glm::vec2{+15.5f, +02.5f}, glm::vec2{+02.5f, +12.5f},
+      glm::vec2{-0.0f, -12.0f}, glm::vec2{-16.0f, +02.5f},
+      glm::vec2{-16.0f, -12.5f}, glm::vec2{-11.7f, -07.5f},
+      glm::vec2{-05.3f, -12.5f}, glm::vec2{-00.0f, -07.5f}, 
+      glm::vec2{+05.3f, -12.5f}, glm::vec2{+11.7f, -07.5f}, 
+      glm::vec2{+16.0f, -12.5f}, glm::vec2{+16.0f, +02.5f},
 
       // Cannon left
       glm::vec2{-12.5f, +10.5f}, glm::vec2{-12.5f, +04.0f},
@@ -44,10 +44,10 @@ void Ship::initializeGL(GLuint program) {
                      1, 2, 3,
                      0, 3, 4,
                      0, 4, 5,
-                     9, 0, 5,
-                     9, 5, 6,
-                     9, 6, 8,
-                     8, 6, 7,
+                     0, 5, 6,
+                     0, 6, 7,
+                     7, 8, 9,
+                     0, 7, 9,
                      // Cannons
                      10, 11, 12,
                      10, 12, 13,
